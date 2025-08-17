206917ec0d1a8eb35d559ce293275cd4c4864594
MD SANAULLAH <msanaullah451@gmail.com>
2025-08-17 21:46:03 +0530

    test01

--- main.py
+++ main.py
@@ -3,11 +3,28 @@ import webbrowser
 import pyttsx3
 
 recognizer = sr.Recognizer()
+
 engine = pyttsx3.init()
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate - 200)
+engine.setProperty('volume', 1)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
 if __name__ == "__main__":
-    speak("Hello, I am your voice assistant.")
+    speak("Initializing Astra....")
+    while True:
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Listening...")
+            audio = r.listen(source, timeout=6, phrase_time_limit=5)
+        print("Recognizing...")
+        try:
+            command = r.recognize_google(audio)
+            print(f"{command}")
+        except Exception as e:
+            print(f"Could not request results; {e}")
