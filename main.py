a4c0be85e8c5b312369a9b6f625387a61882a437
MD SANAULLAH <msanaullah451@gmail.com>
2025-08-19 20:33:41 +0530

    changes

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import speech_recognition as sr
 import webbrowser
 import pyttsx3
 
-recognizer = sr.Recognizer()
-
 engine = pyttsx3.init()
 rate = engine.getProperty('rate')
 engine.setProperty('rate', rate - 200)
@@ -15,6 +13,9 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def processcommand():
+    pass
+
 if __name__ == "__main__":
     speak("Initializing Astra....")
     while True:
@@ -22,16 +23,18 @@ if __name__ == "__main__":
         try:
             with sr.Microphone() as source:
                 print("Listening...")
-                audio = r.listen(source, timeout=4, phrase_time_limit=3)
+                audio = r.listen(source, timeout=3, phrase_time_limit=2)
             print("Recognizing...")
 
             word = r.recognize_google(audio)
+            print(f"You said: {word}")
             if word.lower() == "hello":
-                speak("How can I assist you?")
-                # with sr.Microphone() as source:
-                #     print("Listening for command...")
-                #     audio = r.listen(source, timeout=2, phrase_time_limit=1)
-
+                print("How can I assist you?")
+                with sr.Microphone() as source:
+                    print("Listening for command...")
+                    audio = r.listen(source, timeout=3, phrase_time_limit=2)
+                command = r.recognize_google(audio)
+                
 
         except Exception as e:
             print(f"Error: {e}")
