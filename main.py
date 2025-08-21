be8bfd90d587e6ae613d2a1c4aa968b659828946
MD SANAULLAH <msanaullah451@gmail.com>
2025-08-21 17:12:34 +0530

    uncomplete

--- main.py
+++ main.py
@@ -13,8 +13,16 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-def processcommand():
-    pass
+def processcommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+
 
 if __name__ == "__main__":
     speak("Initializing Astra....")
@@ -28,14 +36,15 @@ if __name__ == "__main__":
 
             word = r.recognize_google(audio)
             print(f"You said: {word}")
-            if word.lower() == "hello":
-                print("How can I assist you?")
+            if word.lower() == "astra":
+                # speak("How can I assist you")
                 with sr.Microphone() as source:
-                    print("Listening for command...")
+                    print("Astra is active...")
                     audio = r.listen(source, timeout=3, phrase_time_limit=2)
-                command = r.recognize_google(audio)
-                
+                    command = r.recognize_google(audio)
+
+                    processcommand(command)
+        
 
         except Exception as e:
             print(f"Error: {e}")
-            continue
